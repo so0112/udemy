1e8b2d6b1cb6c065c9d304ffb08b37178e32e7d1
so0112 <tkddh0112@gmail.com>
2022-11-22 10:53:00 +0900

    React Ts 강의

--- 07-react-ts-starting-project/src/App.tsx
+++ 07-react-ts-starting-project/src/App.tsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
-  return <div className="App"></div>;
+  const todos = [
+    new Todo("리액트 배우기"),
+    new Todo("클래스 배우기"),
+    new Todo("할 거 한 듯"),
+  ];
+  return (
+    <div className="App">
+      <Todos items={todos} />
+    </div>
+  );
 }
 
 export default App;
